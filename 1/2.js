a7f9f784516862e938616ed44bc478f7059e9ab6
Karol Wnuk <k.wnuk@ascetic.pl>
2022-12-02 00:09:05 +0100

    Simplify

--- 1/2.js
+++ 1/2.js
@@ -3,16 +3,7 @@ const fs = require('fs');
 const data = fs.readFileSync('data.txt').toString().split(/\n/).map(val => val === '' ? null : parseInt(val));
 const cals = [];
 
-console.log(data)
-
-data.reduce((acc, current) => {
-  if (current === null) {
-    cals.push(acc)
-    return 0
-  }
-
-  return acc + current
-}, 0)
+data.reduce((acc, current) => current === null ? cals.push(acc) && 0 : acc + current, 0);
 
 const top3 = cals.sort().slice(-3).reduce((acc, curr) => acc + curr);
 
