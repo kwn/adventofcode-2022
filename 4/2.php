c6482e03bf6400b3510771047a967b5d151e1c0f
Karol Wnuk <k.wnuk@ascetic.pl>
2022-12-04 14:15:44 +0100

    Day 4 task 1, 2

---
+++ 4/2.php
@@ -0,0 +1,24 @@
+<?php
+
+$data = array_filter(explode(PHP_EOL, file_get_contents('data.txt')), fn(string $item) => $item !== '');
+$ranges = [];
+
+$ranges = array_map(static function (string $row) {
+    preg_match('/(?<e1f>\d+)-(?<e1t>\d+),(?<e2f>\d+)-(?<e2t>\d+)/', $row, $matches);
+    return [
+        range((int) $matches['e1f'], (int) $matches['e1t']),
+        range((int) $matches['e2f'], (int) $matches['e2t']),
+    ];
+}, $data);
+
+$overlaps = array_reduce($ranges, static function (int $acc, array $pair) {
+    [$e1, $e2] = $pair;
+
+    if (count(array_intersect($e1, $e2)) > 0) {
+        return ++$acc;
+    }
+
+    return $acc;
+}, 0);
+
+var_dump($overlaps);
