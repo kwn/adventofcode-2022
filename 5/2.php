80f4c168c0985b7a4f114f90e3565e934f072de6
Karol Wnuk <k.wnuk@ascetic.pl>
2022-12-05 21:49:11 +0100

    Day 5 task 2

---
+++ 5/2.php
@@ -0,0 +1,31 @@
+<?php
+
+$data = explode(PHP_EOL, file_get_contents('data.txt'));
+
+$stringCrates = array_splice($data, 0, 8);
+$splitCrates = array_map(static fn(string $line) => str_split($line, 4), $stringCrates);
+$parsedCrates = array_map(static fn(array $line) => array_map(static fn(string $crate) => trim($crate) === '' ? null : trim($crate, ' []'), $line), $splitCrates);
+$transposedCrates = array_map(null, ...$parsedCrates);
+$crates = array_map(static fn(array $column) => array_reverse(array_filter($column)), $transposedCrates);
+
+array_splice($data, 0, 2);
+array_splice($data, -1);
+
+$processing = array_map(static function(string $instructions) {
+    preg_match('/move (?<move>\d+) from (?<from>\d+) to (?<to>\d+)/', $instructions, $parsed);
+
+    return [
+        'move' => (int) $parsed['move'],
+        'from' => (int) --$parsed['from'],
+        'to' => (int) --$parsed['to'],
+    ];
+}, $data);
+
+foreach ($processing as $step) {
+    $removed = array_splice($crates[$step['from']], -$step['move']);
+    array_push($crates[$step['to']], ...$removed);
+}
+
+$result = array_reduce($crates, static fn(string $acc, $current) => $acc . end($current), '');
+
+var_dump($result);
